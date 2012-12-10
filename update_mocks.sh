b7cc150ebec429c0cd9099232e91b111910ecec6
Victor Ng <victor@crankycoder.com>
2012-12-10 14:58:50 -0500

    moved mock for statsdclient to the heka_mozsvc_plugins

--- update_mocks.sh
+++ update_mocks.sh
@@ -1,16 +1,17 @@
 #!/bin/sh
+
+# net.Conn
 $GOPATH/bin/mockgen -package="testsupport" \
                     -self_package="heka/testsupport" \
                     -destination="testsupport/mock_net_conn.go" net Conn
 
+# heka.pipeline.Input
 $GOPATH/bin/mockgen -package="pipeline" \
                     -self_package="heka/pipeline" \
                     -destination="pipeline/mock_input_test.go" heka/pipeline Input
 
+# heka.pipeline.OutputWriter
 $GOPATH/bin/mockgen -package="testsupport" \
                     -self_package="heka/testsupport" \
                     -destination="testsupport/mock_output_writer.go" heka/pipeline OutputWriter
 
-$GOPATH/bin/mockgen -package="testsupport" \
-                    -source="../github.com/mozilla-services/heka-mozsvc-plugins/statsdwriter.go" \
-                    -destination="testsupport/mock_statsdclient.go" heka_mozsvc_plugins StatsdClient
